91582759d62739e258e031fd8255638be44cb020
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-08-29 14:07:16 -0400

    Removed react boilerplate and added a basic form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  
+  render() { 
+    return ( 
+      <form className="registration">
+       <h2>Register</h2>
+       <div className="registration__hint">* required field</div>  
+       <div className="form-group">
+         <label htmlFor="name">Name *</label>
+         <input type="text" className="registration__control"
+           name="name" id="name"/>
+       </div>
+       <div className="form-group">
+          <label htmlFor="password">Password *</label>
+          <input type="password" className="registration__control"
+           name="password" id="password"/>
+          <div className="registration__hint">6 to 72 characters, must include a number</div>
+       </div>
+       <div className="form-group">
+         <label htmlFor="repeatPassword">Repeat Password *</label>
+         <input type="password" className="registration__control"
+           name="repeatPassword" id="repeatPassword"/>
+       </div>
 
+       <div className="registration__button__group">
+        <button type="reset" className="registration__button">
+            Cancel
+        </button>
+        <button type="submit" className="registration__button">
+            Save
+        </button>
+       </div>
+     </form>
+     );
+  }
+}
+ 
 export default App;
